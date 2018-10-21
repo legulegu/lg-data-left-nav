445e5a97332bcb204633e2a3fb3a54a18491cf66
Benwei Zhu <benweizhu@gmail.com>
2018-10-21 17:20:34 +0800

    add baidu ad

--- side-nav/NavPanelGroup/index.js
+++ side-nav/NavPanelGroup/index.js
@@ -6,6 +6,7 @@ const NavPanelGroup = props => {
   return (
     <div className={ classNames('lg-side-nav-panel-group', {show: props.show})} >
       {props.children}
+      <div id="lg-side-nav-panel-group-ad" className={ classNames('lg-side-nav-panel-group-ad')}></div>
     </div>
   );
 }
