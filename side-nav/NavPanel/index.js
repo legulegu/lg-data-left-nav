56e037745dc13780190f4c9ed3c0994ccc191e21
benweizhu <benweizhu@gmail.com>
2020-08-06 10:38:11 +0800

    添加广告

--- side-nav/NavPanel/index.js
+++ side-nav/NavPanel/index.js
@@ -6,6 +6,16 @@ const Panel = props => {
   return (
     <div className={classNames('lg-side-nav-panel', { "show": props.show })}>
       {props.children}
+      <div className={classNames('lg-side-nav-panel-ad')}>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <ins className="adsbygoogle"
+          style={{ display: 'inline-block', width: '200px', height: '400px' }}
+          data-ad-client="ca-pub-4841635508396044"
+          data-ad-slot="7158176468"></ins>
+        <script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+      </div>
     </div>
   );
 }
