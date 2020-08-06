34a7dd0183db317415233d1e69f32ec62be80c9d
benweizhu <benweizhu@gmail.com>
2020-08-07 00:26:42 +0800

    add adSense component

--- side-nav/NavPanel/index.js
+++ side-nav/NavPanel/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classNames from "classnames";
+import AdSense from 'react-adsense';
 import "./panel.scss";
 
 const Panel = props => {
@@ -7,14 +8,12 @@ const Panel = props => {
     <div className={classNames('lg-side-nav-panel', { "show": props.show })}>
       {props.children}
       <div className={classNames('lg-side-nav-panel-ad')}>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <ins className="adsbygoogle"
+        <AdSense.Google
+          client='ca-pub-4841635508396044'
+          slot='7158176468'
           style={{ display: 'inline-block', width: '200px', height: '400px' }}
-          data-ad-client="ca-pub-4841635508396044"
-          data-ad-slot="7158176468"></ins>
-        <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
+          format=''
+        />
       </div>
     </div>
   );
