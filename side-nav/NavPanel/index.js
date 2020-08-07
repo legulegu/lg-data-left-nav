d5d06d3208344770504331064c7fdffaeaacadae
benweizhu <benweizhu@gmail.com>
2020-08-07 09:09:52 +0800

    add GoogleAd component

--- side-nav/NavPanel/index.js
+++ side-nav/NavPanel/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import classNames from "classnames";
-import AdSense from 'react-adsense';
+import GoogleAd from "./GoogleAd";
 import "./panel.scss";
 
 const Panel = props => {
@@ -8,7 +8,7 @@ const Panel = props => {
     <div className={classNames('lg-side-nav-panel', { "show": props.show })}>
       {props.children}
       <div className={classNames('lg-side-nav-panel-ad')}>
-        <AdSense.Google
+        <GoogleAd
           client='ca-pub-4841635508396044'
           slot='7158176468'
           style={{ display: 'inline-block', width: '200px', height: '400px' }}
